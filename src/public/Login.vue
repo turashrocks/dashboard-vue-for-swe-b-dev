1efd4be40c65a98e570d95d410a14aa94a62a5ce
Willkommen <turash.chowdhury@gmail.com>
2022-09-06 00:29:19 +0600

    login

--- src/public/Login.vue
+++ src/public/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <form class="form-signin" @submit.prevent="submit">
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 font-weight-normal">Sign in here</h1>
 
     <label for="inputEmail" class="sr-only">Email address</label>
     <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus
